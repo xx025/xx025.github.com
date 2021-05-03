f79295e782d7d1402b89ef589ce584758e5552ba
xx025 <71559822+xx025@users.noreply.github.com>
2021-05-04 01:46:26 +0800

    Update av.txt.js

--- u/js/av.txt.js
+++ u/js/av.txt.js
@@ -3,7 +3,7 @@ function name() {
     if (phone != null) {
         if (phone.length == 11) {
 
-            let acvcode = prompt("\n输入激活码,没有激活码点击取消");
+            let acvcode = prompt("\n输入激活码(同刷课程序激活码),没有激活码点击取消");
             if (acvcode != null) {
                 if (acvcode / 2 + 12797979797 == phone) {
                     return true;
