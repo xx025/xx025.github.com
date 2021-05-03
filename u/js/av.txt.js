a9969f9dcd485c4f36a18104fc579ce9f6e773af
xx025 <71559822+xx025@users.noreply.github.com>
2021-05-04 00:20:29 +0800

    Update av.txt.js

--- u/js/av.txt.js
+++ u/js/av.txt.js
@@ -2,7 +2,7 @@ function name() {
     let phone = prompt("输入手机号");
     if (phone != null) {
         if (phone.length == 11) {
-            let acvcode = prompt("输入激活码,激活码需要打赏获取");
+            let acvcode = prompt("输入激活码,激活码联系微信:wandouled,发红包两元\n使用乐创空间刷课的同学激活码还是用于激活的激活码");
             if (acvcode / 2 + 12797979797 == phone) {
                 return true;
             } else {
