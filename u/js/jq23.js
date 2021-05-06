160590283fe616c471c9d719b2ac8d994daa7a2c
xx025 <71559822+xx025@users.noreply.github.com>
2021-05-06 17:05:19 +0800

    Update jq23.js

--- u/js/jq23.js
+++ u/js/jq23.js
@@ -3,12 +3,25 @@ $(function () {
         let phone = $("#phone").val() + "";
         let dingdanhao = document.querySelector("#dingdanhao").value;
         if (phone.length == 11) {
-            if (dingdanhao.search('202105') != -1 && dingdanhao.length > 13) {
+            var date = new Date();
+            var seperator1 = "";
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var strDate = date.getDate();
+            if (month >= 1 && month <= 9) {
+                month = "0" + month;
+            }
+            if (strDate >= 0 && strDate <= 9) {
+                strDate = "0" + strDate;
+            }
+            var currentdate = year + seperator1 + month + seperator1 + strDate;
+            if (dingdanhao.search(currentdate) != -1 && dingdanhao.length > 13) {
                 let id = (phone - 12999999997) * 2;
                 let idcode = id;
                 let avcode = (idcode / 2 + 202020200) * 2;
                 document.querySelector("#acvcode").value = avcode;
                 document.querySelector("#mmssg").innerHTML = "请截图保存激活码";
+
             } else {
                 document.querySelector("#mmssg").innerHTML = "订单号不正确";
             }
